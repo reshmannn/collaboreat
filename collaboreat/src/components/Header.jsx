5ff7fc68fcec95c707478e5605b682585c8a1c5d
resh <reshma.rajan2714@gmail.com>
2024-02-09 22:54:13 +0530

    app name made big

--- collaboreat/src/components/Header.jsx
+++ collaboreat/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+        <h1 className="font-bold text-sm sm:text-2xl flex flex-wrap">
           <span className="text-slate-500">Collabor</span>
           <span className="text-slate-700">Eat</span>
         </h1>
