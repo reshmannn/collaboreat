0c3766e8e44053dce3df2cf9cdb764c4cde52703
resh <reshma.rajan2714@gmail.com>
2024-02-13 16:15:40 +0530

    signout functionality

--- collaboreat/src/redux/user/userSlice.js
+++ collaboreat/src/redux/user/userSlice.js
@@ -46,6 +46,19 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+
+    signOutUserStart: (state) => {
+      state.loading = true;
+    },
+    signOutUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    signOutUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 
@@ -59,5 +72,8 @@ export const {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserFailure,
+  signOutUserStart,
+  signOutUserSuccess,
 } = userSlice.actions;
 export default userSlice.reducer;
